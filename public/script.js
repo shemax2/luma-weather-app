e7e63dcbd9c3f7879d78d42e30e74d47ce0e5176
Shema Frank <shefrank120@gmail.com>
2025-02-06 12:00:08 +0200

    Add functionality for fetching and displaying love quotes in popup

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+// Search bar functionality
+
 // Select elements
 const searchButton = document.getElementById('search-button');
 const cityInput = document.querySelector('.search-input');
@@ -21,4 +23,36 @@ searchButton.addEventListener('click', async () => {
         console.error("Error fetching weather data:", error.message);
         alert("Someting went wrong");
     }
+});
+
+// Header Surprise Button functionality
+const surpriseButton = document.getElementById('surprise-btn');
+const popup = document.getElementById('popup');
+const closePopup = document.getElementById('close-popup');
+const quoteElement = document.getElementById('quote');
+
+surpriseButton.addEventListener('click', async () => {
+    try{
+        //Fetch love quote from API
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=love', {
+            method: 'GET',
+            headers: {
+                'X-Api-Key': 'TkrD08VqsryRjMedKF4WAg==wfcGfIX7RkusmeUh'
+            }
+        });
+
+        const data = await response.json();
+        const quote = data[0]?.quote || 'Love you, my LIttle Banana!';
+
+        //Display the quote in the popup
+        quoteElement.textContent = quote;
+        popup.style.display = 'block';
+    } catch (error){
+        console.error('Error fetching quote:', error);
+        popup.style.display = 'none';
+    }
+});
+
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none';
 });
